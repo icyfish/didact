80a9a5e47cb3433651376d5d9f19aec3e56a2d14
Fish <bingyuzhang@trip.com>
2020-12-31 15:01:48 +0800

    update

--- README.md
+++ README.md
@@ -3,6 +3,86 @@
 - [original](https://pomb.us/build-your-own-react/)
 - [translation](https://www.notion.so/byzhang/Build-your-own-React-43b3bec3e45c47caa222a8962c9888a0)
 
+### Notes
+
+#### v1:
+
+- 渲染文本节点使用的是 `createTextNode` 而不是 `innerText`, 可以保证在后续处理元素时, 所有元素的形式都是一致的, 抹平差异, 统一处理. 
+
+- 优化: `render` 的过程(见以下代码)会阻碍浏览器的主进程, 比如用户输入或者动画效果等, 因此引入 **concurrent mode** 的概念和 **fiber** 数据结构, 将渲染的过程拆分成多个小单元. 
+```js
+element.props.children.forEach(child => render(child, dom));
+```
+
+#### v2:
+
+- `render`首次执行, workloop 开始,
+
+- `performUnitOfWork_v1`
+
+```js
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+​
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+​
+  const elements = fiber.props.children
+  let index = 0
+	let prevSibling = null
+	
+​	 // prevSibling ?
+	// And we add it to the fiber tree setting it either as a child or as a sibling, depending on whether it’s the first child or not.
+	
+	while (index < elements.length) {
+    const element = elements[index]
+​
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+​
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+​
+    prevSibling = newFiber
+    index++
+  }
+​
+  if (fiber.child) {
+    return fiber.child
+  }
+	let nextFiber = fiber
+	
+	// find child -> sibling -> uncle
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+```
+`performUnitOfWork_v1` 有个问题, 以下操作会导致, 渲染流程结束之前, 浏览器主流程依然被阻滞. 借用 `wipRoot` fiber 来解决这个问题. [performUnitOfWork_v2](./src/index_v2.jsx#L132)
+
+```js
+if (fiber.parent) {
+	fiber.parent.dom.appendChild(fiber.dom)
+}
+```
+
+#### [v3](./src/index.jsx)
+
+
+
 ### TODO
 
-[] add types, remove @types/react
+- [ ] add types,  remove @types/react
--- src/index_v2.jsx
+++ src/index_v2.jsx
@@ -27,6 +27,7 @@ function createDom(fiber) {
       : document.createElement(fiber.type);
 
   updateDom(dom, {}, fiber.props);
+
   return dom;
 }
 
@@ -81,31 +82,18 @@ function commitWork(fiber) {
   if (!fiber) {
     return;
   }
-  // 有了函数式组件之后, 需要不断向上追溯寻找 DOM, commitDeletion 同
-  let domParentFiber = fiber.parent;
-  while (!domParentFiber.dom) {
-    domParentFiber = domParentFiber.parent;
-  }
-  const domParent = domParentFiber.dom;
 
+  const domParent = fiber.parent.dom;
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   } else if (fiber.effectTag === "DELETION") {
-    commitDeletion(fiber, domParent);
+    domParent.removeChild(fiber.dom);
   }
 
   commitWork(fiber.child);
   commitWork(fiber.sibling);
-}
-
-function commitDeletion(fiber, domParent) {
-  if (fiber.dom) {
-    domParent.removeChild(fiber.dom);
-  } else {
-    commitDeletion(fiber.child, domParent);
-  }
 }
 
 function render(element, container) {
@@ -139,16 +127,16 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop);
 }
 
-// 主进程空闲时调用的函数 web api
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  const isFunctionComponent = fiber.type instanceof Function;
-  if (isFunctionComponent) {
-    updateFunctionComponent(fiber);
-  } else {
-    updateHostComponent(fiber);
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
   }
+
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
+
   if (fiber.child) {
     return fiber.child;
   }
@@ -159,55 +147,6 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
-}
-
-let wipFiber = null;
-let hookIndex = null;
-
-function updateFunctionComponent(fiber) {
-  wipFiber = fiber;
-  hookIndex = 0;
-  wipFiber.hooks = [];
-  const children = [fiber.type(fiber.props)];
-  reconcileChildren(fiber, children);
-}
-
-function updateHostComponent(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-  reconcileChildren(fiber, fiber.props.children);
-}
-
-function useState(initial) {
-  const oldHook =
-    wipFiber.alternate &&
-    wipFiber.alternate.hooks &&
-    wipFiber.alternate.hooks[hookIndex];
-  const hook = {
-    state: oldHook ? oldHook.state : initial,
-    queue: []
-  };
-
-  const actions = oldHook ? oldHook.queue : [];
-  actions.forEach(action => {
-    hook.state = action(hook.state);
-  });
-
-  const setState = action => {
-    hook.queue.push(action);
-    wipRoot = {
-      dom: currentRoot.dom,
-      props: currentRoot.props,
-      alternate: currentRoot
-    };
-    nextUnitOfWork = wipRoot;
-    deletions = [];
-  };
-
-  wipFiber.hooks.push(hook);
-  hookIndex++;
-  return [hook.state, setState];
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -250,6 +189,7 @@ function reconcileChildren(wipFiber, elements) {
       oldFiber = oldFiber.sibling;
     }
 
+    console.log(prevSibling, 'prevSibling')
     if (index === 0) {
       wipFiber.child = newFiber;
     } else if (element) {
@@ -263,15 +203,24 @@ function reconcileChildren(wipFiber, elements) {
 
 const Didact = {
   createElement,
-  render,
-  useState
+  render
 };
 
 /** @jsx Didact.createElement */
-function Counter() {
-  const [state, setState] = Didact.useState(1);
-  return <h1 onClick={() => setState(c => c + 1)}>Count: {state}</h1>;
-}
-const element = <Counter />;
 const container = document.getElementById("root");
-Didact.render(element, container);
+
+const updateValue = e => {
+  rerender(e.target.value);
+};
+
+const rerender = value => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+  Didact.render(element, container);
+};
+
+rerender("World");
